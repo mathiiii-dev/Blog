27fd4bbdf0cd65dbfeaa0e9fca992efde28b9495
mathias73 <mat.micheli99@gmail.com>
2020-10-30 10:10:58 +0100

    change answer style

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -4,7 +4,10 @@ namespace App\Model;
 
 class Config
 {
+
     const DB_HOST = 'mysql:dbname=blog;host=127.0.0.1';
     const DB_USER = 'root';
     const DB_PASSWORD = '';
+    const DB_OPTION = [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+
 }
